58a03ba214f04d3afcbdbb8c4c470c1be1614101
antgal <gallet.antoine@gmail.com>
2014-10-15 13:00:20 +0200

    .

--- Tukui/Core/Init.lua
+++ Tukui/Core/Init.lua
@@ -27,3 +27,86 @@ SLASH_RELOADUI1 = "/rl"
 SlashCmdList.RELOADUI = ReloadUI
 
 Tukui = Engine
+
+if (Engine[1].ScreenWidth > 3840) or (UIParent:GetWidth() + 1 > Engine[1].ScreenWidth) then
+	local width = Engine[1].ScreenWidth
+	local height = Engine[1].ScreenHeight
+	
+	-- because some user enable bezel compensation, we need to find the real width of a single monitor.
+	-- I don't know how it really work, but i'm assuming they add pixel to width to compensate the bezel. :P
+	
+	-- HQ resolution
+	if width >= 9840 then width = 3280 end                   	                -- WQSXGA
+	if width >= 7680 and width < 9840 then width = 2560 end                     -- WQXGA
+	if width >= 5760 and width < 7680 then width = 1920 end 	                -- WUXGA & HDTV
+	if width >= 5040 and width < 5760 then width = 1680 end 	                -- WSXGA+
+	
+	-- adding height condition here to be sure it work with bezel compensation because WSXGA+ and UXGA/HD+ got approx same width
+	if width >= 4800 and width < 5760 and height == 900 then width = 1600 end   -- UXGA & HD+
+	
+	-- low resolution screen
+	if width >= 4320 and width < 4800 then width = 1440 end 	                -- WSXGA
+	if width >= 4080 and width < 4320 then width = 1360 end 	                -- WXGA
+	if width >= 3840 and width < 4080 then width = 1224 end 	                -- SXGA & SXGA (UVGA) & WXGA & HDTV
+	
+	-- register a constant, we will need it later for launch.lua
+	Engine[1].eyefinity = width
+end
+
+--------------------------------------------------------
+-- Auto Scale UI (Overwrite Settings and Blizzard)
+--------------------------------------------------------
+
+-- autoscale
+if C["general"].autoscale == true then
+	C["general"].uiscale = min(2, max(.64, 768/string.match(Engine[1].Resolution, "%d+x(%d+)")))
+end
+	
+--------------------------------------------------------
+-- Graphics Settings
+--------------------------------------------------------
+
+-- the ui doesn't reload if ratio stay the same, we need to force reload if it happen.
+local function NeedReloadUI()
+	local resolution = Graphics_ResolutionDropDown
+	local x, y = resolution:getValues()
+	local oldratio = Engine[1].ScreenWidth / Engine[1].ScreenHeight
+	local newratio = x / y
+	local oldreso = Engine[1].Resolution
+	local newreso = x.."x"..y
+	
+	if (oldratio == newratio) and (oldreso ~= newreso) then
+		ReloadUI()
+	end
+end
+
+local Graphic = CreateFrame("Frame")
+Graphic:RegisterEvent("PLAYER_ENTERING_WORLD")
+Graphic:SetScript("OnEvent", function(self, event)
+	-- we adjust UIParent to screen #1 if Eyefinity is found
+	if Engine[1].eyefinity then
+		local width = Engine[1].eyefinity
+		local height = Engine[1].ScreenHeight
+		
+		-- if autoscale is off, find a new width value of UIParent for screen #1.
+		if not C.general.autoscale or height > 1200 then
+			local h = UIParent:GetHeight()
+			local ratio = Engine[1].ScreenHeight / h
+			local w = Engine[1].eyefinity / ratio
+			
+			width = w
+			height = h			
+		end
+		
+		UIParent:SetSize(width, height)
+		UIParent:ClearAllPoints()
+		UIParent:SetPoint("CENTER")		
+	end
+
+	-- require a reload when graphics option changes, even if Standard Blizzard UI doesn't really need it.
+	VideoOptionsFrameOkay:HookScript("OnClick", NeedReloadUI)
+	VideoOptionsFrameApply:HookScript("OnClick", NeedReloadUI)
+	
+	-- unload
+	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+end)
